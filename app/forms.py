dfacd2171b1fcd882f822adf92117b8e9e55e989
Simrah A <77649156+simraha@users.noreply.github.com>
2022-04-27 16:36:22 -0400

    Update forms.py
    Updated forms

--- app/forms.py
+++ app/forms.py
@@ -12,3 +12,35 @@ class User_Login(forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'password') 
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name',
+                  'last_name', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(UserRegisterForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+       
+
+class EditProfileForm(forms.ModelForm):
+    username = forms.CharField(required=True)
+    class Meta:
+        model = User
+        fields = ('username', 'email')
